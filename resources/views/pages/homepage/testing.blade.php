89c605885b57f65497b34f8e8b32777c26af03cc
Ram Emerson Goria <goria.reg@gmail.com>
2024-09-14 02:50:35 +0800

    Testing2

--- resources/views/pages/homepage/testing.blade.php
+++ resources/views/pages/homepage/testing.blade.php
@@ -15,7 +15,7 @@
 </head>
 <body>
     <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark fixed-top">
         <a class="navbar-brand" href="#">MySite</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -32,6 +32,27 @@
                     <a class="nav-link" href="#">Contact</a>
                 </li>
             </ul>
+        </div>
+    </nav>
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark bg-ji">
+        <div class="container">
+            <a class="navbar-brand d-none d-lg-block" href="mailto:consult@avvanz.com">
+                <i class="fas fa-envelope text-marigold"></i>
+                consult@avvanz.com
+            </a>
+            <div class="navbar-collapse">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link login-1 text-uppercase fw-bolder" href="https://www.avvanz.com/screenglobal/">ScreenGlobal LOGIN</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link login-2 text-uppercase fw-bolder" href="https://www.avvanz.com/cddglobal/">CDDGLOBAL LOGIN</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link login-3 text-uppercase fw-bolder" href="https://learnglobal.apptitude.xyz/">LEARNGLOBAL LOGIN</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </nav>
 
