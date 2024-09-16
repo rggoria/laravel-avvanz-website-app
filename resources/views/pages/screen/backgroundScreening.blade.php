8406e75b3c2246cd1436f03876b1958b58dbab5a
Ram Emerson Goria <goria.reg@gmail.com>
2024-09-16 16:24:09 +0800

    Testing1.37

--- resources/views/pages/screen/backgroundScreening.blade.php
+++ resources/views/pages/screen/backgroundScreening.blade.php
@@ -11,14 +11,14 @@ Background Screening - Avvanz Global
 
 <!-- Hero Section -->
 <section class="bg-image">
-    <div class="container">
+    <div class="container hero-padding">
         <div class="row text-white">
             <div class="col-md-7 offset-auto">
                 <h1 class="fw-bolder display-5">
                     Background Screening
                 </h1>
                 <h1 class="divider-start-25"></h1>
-                <p class="lead">
+                <p>
                     Get started
                 </p>
             </div>
@@ -27,18 +27,18 @@ Background Screening - Avvanz Global
 </section>
 
 <!-- Checks Screening -->
-<section class="bg-mg py-5 text-white">
+<section class="bg-mg hero-padding text-white">
     <div class="text-center">
         <h1 class="fw-bolder display-5">
             Individual Background Checks Screening
         </h1>
         <h1 class="divider-center-25"></h1>
     </div>
-    <div class="container py-5">
-        <p class="lead">
+    <div class="container container-margin">
+        <p>
             Statistics have shown that more than 30% of CVs or resumes have discrepancies. Hence it’s imperative that a thorough Background Screening and Due Diligence process is in place to sieve out the wrong hires who can be detrimental to your organization. Your candidates and employees should be on the highest level of “integrity” and “security”. Both aspects cannot be compromised otherwise it might cost your organization in terms of reduced productivity, fraud-related costs, reputational damages, and even a decline in shareholder earnings.
         </p>
-        <p class="lead">
+        <p>
             Since the discrepancies can be in several areas, the candidates must be channeled through different background checkpoints based on the risks associated with their functions or roles. A thorough background screening process will greatly mitigate the “security” and “integrity” risks. Organizations need to understand the following when implementing a background screening strategy:
         </p>
         <ul class="list-group ms-4">
@@ -51,7 +51,7 @@ Background Screening - Avvanz Global
                 PBSA Council ISO27001, and ISO27701 Certified
             </li>
         </ul>
-        <p class="lead">
+        <p>
             This is really because background screening is an enormous task given the diverse global backgrounds of candidates, inherent risks, and complexity of country-specific personal data-related laws and regulations.
         </p>
     </div>
