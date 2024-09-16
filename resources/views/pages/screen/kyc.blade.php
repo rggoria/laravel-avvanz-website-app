945959113ce26c81c18592509d11201949d81c15
Ram Emerson Goria <goria.reg@gmail.com>
2024-09-16 17:23:10 +0800

    Testing1.44

--- resources/views/pages/screen/kyc.blade.php
+++ resources/views/pages/screen/kyc.blade.php
@@ -11,14 +11,14 @@ KYC AML Compliance - Avvanz Global
 
 <!-- Hero Section -->
 <div class="bg-image">
-    <div class="container">
+    <div class="container padding-vertical">
         <div class="row text-white">
             <div class="col-md-7 offset-auto">
                 <h1 class="fw-bolder display-5">
                     KYC AML Compliance
                 </h1>
                 <h1 class="divider-start-25"></h1>
-                <p class="lead">
+                <p>
                     Building Stronger Businesses
                 </p>
             </div>
@@ -27,47 +27,56 @@ KYC AML Compliance - Avvanz Global
 </div>
 
 <!-- Award Section -->
-<section class="bg-mg py-5">
-    <div class="container text-center text-white w-75">
-        <p class="lead">
+<section class="bg-mg padding-vertical">
+    <div class="container text-center text-white">
+        <p>
             Gold Medal awarded ScreenGlobal Platform allows for Know Your Customer (KYC) and Customer/Company Due Diligence (CDD) related automated workflows that provide quick visibility into potential risks associated with financial crimes like money laundering and terrorist financing. Our comprehensive fraud detection and prevention solutions provide dynamic insights into customer events and changes.
         </p>
     </div>
 </section>
 
 <!-- Partner Section -->
-<section class="container my-5 text-white">
+<section class="container margin-vertical">
     <div class="row g-3 py-3">
-        <div class="col-sm-12 col-md-4 col-lg-4 mb-4">
+        <div class="col-sm-12 col-md-12 col-lg-4 mb-4">
             <div class="card p-5 d-flex justify-content-center align-items-center bg-marigold h-100 w-100">
-                <div class="card-body text-center">
-                    <img src="{{ asset('images/screen/kyc1.png') }}" class="card-img-top img-fluid rounded mx-auto d-block" alt="Best Background Screening Provider">
-                    <br>
-                    <h4 class="card-title fw-bolder">Verify identity of your subjects</h4>
+                <div class="card-body text-center text-white">
+                    <img 
+                        src="{{ asset('images/screen/kyc1.webp') }}" 
+                        class="card-img-top img-fluid rounded mx-auto d-block" 
+                        alt="KYC Image 1"
+                        loading="lazy">
+                    <h4 class="card-title mt-3 fw-bolder">Verify identity of your subjects</h4>
                     <p class="card-text">
                         Avvanz’s Digital Identity Verification partnerships across the globe enable you to authenticate and ensure your subject is really who (s)he claims to be.
                     </p>
                 </div>
             </div>
         </div>
-        <div class="col-sm-12 col-md-4 col-lg-4 mb-4">
+        <div class="col-sm-12 col-md-12 col-lg-4 mb-4">
             <div class="card p-5 d-flex justify-content-center align-items-center bg-mg h-100 w-100">
-                <div class="card-body text-center">
-                    <img src="{{ asset('images/screen/kyc2.png') }}" class="card-img-top img-fluid rounded mx-auto d-block" alt="Best Background Screening Provider">
-                    <br>
-                    <h4 class="card-title fw-bolder">KYC or Know Your Customer (or Business or Partner or Employee)</h4>
+                <div class="card-body text-center text-white">
+                    <img 
+                        src="{{ asset('images/screen/kyc2.webp') }}" 
+                        class="card-img-top img-fluid rounded mx-auto d-block" 
+                        alt="KYC Image 2"
+                        loading="lazy">
+                    <h4 class="card-title mt-3 fw-bolder">KYC or Know Your Customer (or Business or Partner or Employee)</h4>
                     <p class="card-text">
                         Avvanz’s CDDGlobal and ScreenGlobal will offer you full insights on your subject before you engage in a partnership with that subject. We have APIs available too.
                     </p>
                 </div>
             </div>
         </div>
-        <div class="col-sm-12 col-md-4 col-lg-4 mb-4">
+        <div class="col-sm-12 col-md-12 col-lg-4 mb-4">
             <div class="card p-5 d-flex justify-content-center align-items-center bg-marigold h-100 w-100">
-                <div class="card-body text-center">
-                    <img src="{{ asset('images/screen/kyc3.png') }}" class="card-img-top img-fluid rounded mx-auto d-block" alt="Best Background Screening Provider">
-                    <br>
-                    <h4 class="card-title fw-bolder">Global Watchlist and Sanctions with constant monitoring tools</h4>
+                <div class="card-body text-center text-white">
+                    <img 
+                        src="{{ asset('images/screen/kyc3.webp') }}" 
+                        class="card-img-top img-fluid rounded mx-auto d-block" 
+                        alt="KYC Image 3"
+                        loading="lazy">
+                    <h4 class="card-title mt-3 fw-bolder">Global Watchlist and Sanctions with constant monitoring tools</h4>
                     <p class="card-text">
                         Avvanz’s checks are focused around AML (Anti-money laundering), CTF (Counter-terrorism Financing), PEP (Politically Exposed Persons), Sanctions Compliance, Adverse Media and other crimes such as bribery and corruption, fraud and modern slavery.
                     </p>
@@ -105,7 +114,11 @@ KYC AML Compliance - Avvanz Global
                 </div>
             </div>
             <div class="col-md-5 d-flex align-items-center justify-content-center">
-                <img src="{{ asset('images/screen/kyc4.svg') }}" alt="Placeholder Image" class="transition-up img-fluid rounded-3 w-auto h-auto">
+                <img 
+                    src="{{ asset('images/screen/kyc4.webp') }}" 
+                    class="transition-up img-fluid rounded-3 w-auto h-auto" 
+                    alt="KYC Image 4"
+                    loading="lazy">
             </div>
         </div>
     </div>
