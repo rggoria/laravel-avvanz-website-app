131c3a61bd5caf88b4de4968a03e6f620565919a
Ram Emerson Goria <goria.reg@gmail.com>
2024-09-16 17:02:55 +0800

    Testing1.40

--- resources/views/pages/screen/avvanzSreenGlobal.blade.php
+++ resources/views/pages/screen/avvanzSreenGlobal.blade.php
@@ -11,14 +11,14 @@ Avvanz ScreenGlobal - Avvanz Global
 
 <!-- Hero Section -->
 <div class="bg-image">
-    <div class="container">
+    <div class="container padding-vertical">
         <div class="row text-white">
             <div class="col-md-7 offset-auto">
                 <h1 class="fw-bolder display-5">
                     Avvanz ScreenGlobal
                 </h1>
                 <h1 class="divider-start-25"></h1>
-                <p class="lead">
+                <p>
                     Your Trusted Partner
                 </p>
             </div>
@@ -27,7 +27,7 @@ Avvanz ScreenGlobal - Avvanz Global
 </div>
 
 <!-- CVs or Resumes Section -->
-<section class="bg-mg py-5">
+<section class="bg-mg padding-vertical">
     <div class="container">
         <div class="row">
             <div class="col-md-6 d-flex align-items-center">
@@ -36,13 +36,17 @@ Avvanz ScreenGlobal - Avvanz Global
                         More than 20% of CVs or Resumes
                     </h1>
                     <h1 class="divider-start-25 mb-3"></h1>
-                    <p class="lead">
+                    <p>
                         Statistics have shown that more than 20% of CVs or resumes have discrepancies and hence it’s imperative that a thorough Background Screening and Due Diligence process is in place to sieve out the wrong hires who can be detrimental to your organization. Your candidates and employees need to be of the highest level of “integrity” and “security”. Both aspects cannot be compromised otherwise it might cost your organization in terms of reduced productivity, fraud-related costs, reputational damages, and even a decline in shareholder earnings.
                     </p>
                 </div>
             </div>
             <div class="col-md-6 d-flex align-items-center justify-content-center">
-                <img src="{{ asset('images/screen/screenglobal.svg') }}" alt="Placeholder Image" class="transition-up img-fluid rounded-3 w-auto h-auto">
+                <img
+                    src="{{ asset('images/screen/screenglobal.webp') }}"
+                    alt="Screen Global"
+                    class="transition-up img-fluid rounded-3 w-auto h-auto"
+                    loading="lazy">
             </div>
         </div>
     </div>
@@ -50,13 +54,13 @@ Avvanz ScreenGlobal - Avvanz Global
 
 <!-- Strategy Section -->
 <section class="container py-5">
-    <p class="lead">
+    <p>
         Since the discrepancies can be in several areas, the candidates must be channeled through different background checkpoints based on the risks associated with their functions or roles. A thorough background screening process will greatly mitigate the “security” and “integrity” risks. Organizations need to understand the following when implementing a background screening strategy:
     </p>
     <div class="row g-3 text-white">
         <div class="col-6 py-5">
             <div class="p-5 d-flex justify-content-center align-items-center bg-mg fw-bolder text-center h-100 w-100">
-                <div class="mb-3 lead fw-bold">
+                <div class="mb-3 fw-bold">
                     <i class="fas fa-question fa-5x"></i>
                     <p class="mt-3 ">Why and how companies should conduct background screening?</p>
                 </div>                
@@ -64,7 +68,7 @@ Avvanz ScreenGlobal - Avvanz Global
         </div>
         <div class="col-6 py-5">
             <div class="p-5 d-flex justify-content-center align-items-center bg-marigold fw-bolder text-center h-100 w-100">
-                <div class="mb-3 lead fw-bold">
+                <div class="mb-3 fw-bold">
                     <i class="fas fa-lightbulb fa-5x"></i>
                     <p class="mt-3">What to look out for when working with the right background screening partner</p>
                 </div>                
