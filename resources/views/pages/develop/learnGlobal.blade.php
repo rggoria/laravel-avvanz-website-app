2e23731a18021f3ea689f863a66dc9f7051dd627
Ram Emerson Goria <goria.reg@gmail.com>
2024-09-17 16:16:41 +0800

    Testing1.96

--- resources/views/pages/develop/learnGlobal.blade.php
+++ resources/views/pages/develop/learnGlobal.blade.php
@@ -10,7 +10,7 @@ LearnGlobal - Avvanz Global
 @include('layouts.navbar')
 
 <!-- Hero Section -->
-<section class="bg-image-1">
+<section class="bg-image-2">
     <div class="container padding-vertical">
         <div class="row text-white">
             <div class="col-md-7 offset-auto">
