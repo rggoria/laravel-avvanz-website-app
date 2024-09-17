5c0c202a19b8b5ce0cccf69a59e9b96682c77cb1
Ram Emerson Goria <goria.reg@gmail.com>
2024-09-17 10:38:53 +0800

    Testing1.50

--- resources/views/pages/homepage/testing.blade.php
+++ resources/views/pages/homepage/testing.blade.php
@@ -7,7 +7,9 @@
     
     <!-- Preload Bootstrap CSS -->
     <link rel="preload" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" as="style">
-    
+    <!-- Font Awesome CSS CDN -->
+    <link rel="preload" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" as="style">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}" media="print" onload="this.media='all'">
     <!-- Minified Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     
@@ -18,10 +20,7 @@
     </style>
 </head>
 <body>
-    <div class="container">
-        <h1 class="mt-5">Optimized Bootstrap Page</h1>
-        <!-- Your content here -->
-    </div>
+    @include('layouts.navbar')
     
     <!-- Deferred Bootstrap JavaScript -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" defer></script>
