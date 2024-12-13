e82d5964add9c10db3c201f1149c5b436dc87715
Ram Emerson Goria <goria.reg@gmail.com>
2024-12-13 17:40:01 +0800

    homepage image

--- resources/views/UATWEB/pages/homepage/index.blade.php
+++ resources/views/UATWEB/pages/homepage/index.blade.php
@@ -66,9 +66,9 @@ Avvanz - Background Checks & Screening
             </div>
             <div class="col-md-6 order-1 order-md-2 justify-content-center align-content-center text-center">
                 <picture>
-                    <source srcset="{{ asset('images/homepage/homepage1.webp') }}" type="image/webp">
-                    <source srcset="{{ asset('images/homepage/homepage1.jpg') }}" type="image/jpeg">
-                    <img src="{{ asset('images/homepage/homepage1.jpg') }}" alt="Homepage Image 1" class="img-fluid transition-up" width="300" height="300">
+                    <source srcset="{{ asset('images/homepage/homepage1-m.webp') }}" type="image/webp" media="(max-width: 767px)">
+                    <source srcset="{{ asset('images/homepage/homepage1-w.webp') }}" type="image/webp" media="(min-width: 768px)">
+                    <img src="{{ asset('images/homepage/homepage1-w.webp') }}" alt="Homepage Image" class="img-fluid transition-up" width="300" height="300">
                 </picture>
             </div>
         </div>            
