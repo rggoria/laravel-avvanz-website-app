c6db0a9b135a6ba498e957075b1c8727db32d61f
Ram Emerson Goria <goria.reg@gmail.com>
2024-12-23 17:07:54 +0800

    testing: floating btn v3

--- resources/views/UATWEB/partials/floating.blade.php
+++ resources/views/UATWEB/partials/floating.blade.php
@@ -3,6 +3,8 @@
     <img src="{{ asset('images/homepage/avvanzFooter.webp') }}" alt="Left Icon" loading="lazy">
 </a> --}}
 <!-- Right -->
-<a href="https://api.whatsapp.com/send/?phone=639688731707&text&type=phone_number&app_absent=0" class="floating-btn floating-btn-right text-decoration-none">
-    <i class="fab fa-whatsapp fa-2x text-white"></i>
+<a href="https://api.whatsapp.com/send/?phone=639688731707&text&type=phone_number&app_absent=0" 
+   class="floating-btn floating-btn-right text-decoration-none"
+   aria-label="Contact via WhatsApp" tabindex="0">
+   <i class="fab fa-whatsapp fa-2x text-white"></i>
 </a>
