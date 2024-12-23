6593a38e2e6db0f3503793dbc97fed710e1bab85
Ram Emerson Goria <goria.reg@gmail.com>
2024-12-23 13:15:25 +0800

    testing: navbar

--- resources/views/UATWEB/layouts/testingLayouts.blade.php
+++ resources/views/UATWEB/layouts/testingLayouts.blade.php
@@ -8,8 +8,14 @@
     <meta name="description" content="Our Multi-award winning background checks platform ScreenGlobal will help you mitigate risk due to wrongful hires.">
     <!-- Website Icon -->
     <link rel="icon" type="image/x-icon" href="{{ asset('images/homepage/avvanz.ico') }}"/>
+
+    <!-- Minified Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
+    <!-- Navbar -->
+    @include('UATWEB.partials.navbar')
+
     @yield('content')
 
      <!-- This site is converting visitors into subscribers and customers with https://respond.io -->
