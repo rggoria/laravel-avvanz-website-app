856736c73a171b6c6d9180fe90e02664ced1441a
Ram Emerson Goria <goria.reg@gmail.com>
2024-12-24 15:01:57 +0800

    testing:

--- resources/views/UATWEB/layouts/testingLayouts.blade.php
+++ resources/views/UATWEB/layouts/testingLayouts.blade.php
@@ -6,26 +6,18 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
     <meta name="description" content="Our Multi-award winning background checks platform ScreenGlobal will help you mitigate risk due to wrongful hires.">
-    
     <!-- Website Icon -->
     <link rel="icon" type="image/x-icon" href="{{ asset('images/homepage/avvanz.ico') }}"/>
-    
-    <!-- Preload critical CSS -->
+    <!-- Preload Bootstrap CSS -->
     <link rel="preload" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" as="style" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="preload" href="{{ asset('css/style.css') }}" as="style">
-    
-    <!-- Preload Google Fonts with font-display swap -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,400i,700,700i&display=swap" />
-    
     <!-- Minified Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
         integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-    
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+        crossorigin="anonymous" referrerpolicy="no-referrer" media="print" onload="this.media='all'" />
+
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}" media="print" onload="this.media='all'">
     @yield('css')
 </head>
 <body>
@@ -40,18 +32,15 @@
     <!-- Footer Section -->
     @include('UATWEB.partials.footer')
 
-    <!-- Webchat -->
+    <!-- This site is converting visitors into subscribers and customers with https://respond.io -->
     <script defer id="respondio__widget" src="https://cdn.respond.io/webchat/widget/widget.js?cId=6850f5d54cbfd25be536b0b59847be5"></script>
-
-    <!-- jQuery CDN (async) -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" async></script>
+    <!-- https://respond.io -->
 
     <!-- Custom JS -->
     <script src="{{ asset('js/index.js') }}" defer></script>
 
     <!-- Deferred Bootstrap JavaScript -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" defer></script>
-
     @yield('scripts')
 </body>
 </html>
