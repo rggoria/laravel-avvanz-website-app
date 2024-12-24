35861f07d837f581a6ffadef41e1bde15d130856
Ram Emerson Goria <goria.reg@gmail.com>
2024-12-24 17:37:35 +0800

    testing: critical css

--- resources/views/UATWEB/layouts/testingLayouts.blade.php
+++ resources/views/UATWEB/layouts/testingLayouts.blade.php
@@ -20,6 +20,77 @@
         integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
         crossorigin="anonymous" referrerpolicy="no-referrer" media="print" onload="this.media='all'" />
 
+    <!-- Critical CSS -->
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Helvetica Neue", Arial, sans-serif;
+        }
+        .bg-image-1 {
+            background: url("../images/homepage/homepageBg.webp") no-repeat center;
+            background-size: cover;
+            position: relative;
+            min-height: 300px
+        }
+        .bg-image-2 {
+            background: url("../images/homepage/homepageBg.webp") no-repeat center;
+            background-size: cover;
+            position: relative;
+            min-height: 100px;
+        }
+        .bg-image-christmas {
+            background: url("../images/homepage/avvanzBgChristmas2.webp") no-repeat center center;
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-position: center;
+        }
+
+        .padding-vertical {
+            padding-top: 3rem;
+            padding-bottom: 3rem;
+        }
+        .margin-vertical {
+            margin-top: 3rem;
+            margin-bottom: 3rem;
+        }
+        .section-padding {
+            padding: 3rem;
+        }
+        .text-white {
+            color: white;
+        }
+        .text-center {
+            text-align: center;
+        }
+        /* Cookie consent styles */
+        .cookie-consent-banner {
+            position: fixed;
+            bottom: 0;
+            left: 0;
+            width: 100%;
+            background-color: #333;
+            color: #fff;
+            text-align: center;
+            padding: 10px 0;
+            font-size: 16px;
+            display: none;
+            z-index: 9999;
+        }
+        .accept-cookies-btn, .reject-cookies-btn {
+            background-color: green;
+            color: #fff;
+            padding: 5px 10px;
+            font-size: 16px;
+            border: none;
+            cursor: pointer;
+            margin: 5px;
+        }
+        .reject-cookies-btn {
+            background-color: red;
+        }
+    </style>
+
     <link rel="stylesheet" href="{{ asset('css/style.css') }}" media="print" onload="this.media='all'">
     @yield('css')
 </head>
