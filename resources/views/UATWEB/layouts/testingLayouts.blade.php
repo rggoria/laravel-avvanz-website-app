d45efa225cb10811d19cf86044fe5db358c880af
Ram Emerson Goria <goria.reg@gmail.com>
2024-12-24 15:46:23 +0800

    testing: remove footer

--- resources/views/UATWEB/layouts/testingLayouts.blade.php
+++ resources/views/UATWEB/layouts/testingLayouts.blade.php
@@ -10,8 +10,9 @@
     <!-- Website Icon -->
     <link rel="icon" type="image/x-icon" href="{{ asset('images/homepage/avvanz.ico') }}"/>
 
-    <!-- Preload critical CSS -->
+    <!-- Preload Critical CSS -->
     <link rel="preload" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" as="style" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="preload" href="{{ asset('css/style.css') }}" as="style">
     
     <!-- Preload Google Fonts with font-display swap -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,400i,700,700i&display=swap" />
@@ -46,12 +47,13 @@
     <script defer id="respondio__widget" src="https://cdn.respond.io/webchat/widget/widget.js?cId=6850f5d54cbfd25be536b0b59847be5"></script>
 
     <!-- Core JavaScript Libraries -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" async></script>
+    <!-- jQuery CDN (Do not use async; it's critical for other scripts) -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
-    <!-- Deferred Bootstrap JavaScript -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" defer></script>
+    <!-- Bootstrap JavaScript (with defer to prevent render blocking) -->
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
-    <!-- Custom JavaScript (Deferred to load last) -->
+    <!-- Custom JavaScript (Defer to load last, after everything else) -->
     <script src="{{ asset('js/index.js') }}" defer></script>
 
     @yield('scripts') <!-- Allow additional scripts from specific pages -->
