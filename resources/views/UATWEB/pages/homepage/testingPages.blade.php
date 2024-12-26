bd0fd722b8d85e1f575e29be13324fc4aaa1de5e
Ram Emerson Goria <goria.reg@gmail.com>
2024-12-26 13:22:54 +0800

    testing: css and 1st hero

--- resources/views/UATWEB/pages/homepage/testingPages.blade.php
+++ resources/views/UATWEB/pages/homepage/testingPages.blade.php
@@ -26,12 +26,7 @@ Avvanz - Background Checks & Screening
                 </div>
             </div>
             <div class="col-md-6 order-1 order-md-2 justify-content-center align-content-center text-center">
-                <picture>
-                    <source srcset="{{ asset('images/homepage/homepage1-m.webp') }}" type="image/webp" media="(max-width: 767px)">
-                    <source srcset="{{ asset('images/homepage/homepage1-w.webp') }}" type="image/webp" media="(min-width: 768px)">
-                    <img src="{{ asset('images/homepage/homepage1-m.webp') }}" alt="Avvanz homepage image showcasing background check services" class="img-fluid transition-up" width="360" height="360">
-                </picture>
-                
+                <img src="{{ asset('images/homepage/homepage1-w.webp') }}" alt="Avvanz homepage image showcasing background check services" class="img-fluid transition-up">
             </div>
         </div>
     </div>
