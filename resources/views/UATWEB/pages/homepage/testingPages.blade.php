541c25c0885dcb3ac90f769751f33fb0d54aab1c
Ram Emerson Goria <goria.reg@gmail.com>
2024-12-27 15:28:20 +0800

    testing: 7th section

--- resources/views/UATWEB/pages/homepage/testingPages.blade.php
+++ resources/views/UATWEB/pages/homepage/testingPages.blade.php
@@ -255,12 +255,12 @@ Avvanz - Background Checks & Screening
     <div class="container">
         <div class="row g-3">
             <div class="col-lg-4 p-1">
-                <div class="card">
+                <div class="card-display card-border">
                     <div class="card-content p-5">
                         <div class="card-body d-flex flex-column align-items-center text-center">
                             <div class="card-icon d-flex justify-content-center align-items-center">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" width="32" height="32">
-                                    <path d="M0 24C0 10.7 10.7 0 24 0L69.5 0c22 0 41.5 12.8 50.6 32l411 0c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3l-288.5 0 5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5L488 336c13.3 0 24 10.7 24 24s-10.7 24-24 24l-288.3 0c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5L24 48C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/>
+                                    <path fill="white" d="M0 24C0 10.7 10.7 0 24 0L69.5 0c22 0 41.5 12.8 50.6 32l411 0c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3l-288.5 0 5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5L488 336c13.3 0 24 10.7 24 24s-10.7 24-24 24l-288.3 0c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5L24 48C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/>
                                 </svg>
                             </div>
                             <p class="card-title fs-4 fw-bold mt-5">Banking, Financial Services & Insurance</p>
