10c609a4ccabcd86a684c5723895047725b10ecb
Ram Emerson Goria <goria.reg@gmail.com>
2024-12-29 02:40:42 +0800

    testing: navbar

--- resources/views/UATWEB/pages/homepage/testingPages.blade.php
+++ resources/views/UATWEB/pages/homepage/testingPages.blade.php
@@ -4,10 +4,13 @@
 Avvanz - Background Checks & Screening
 @endsection
 
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/homepageTesting.css') }}">
+@endsection
 
 @section('content')
     <!-- Navbar -->
     @include('UATWEB.partials.navbar')
-    
+
     Hello World
 @endsection
