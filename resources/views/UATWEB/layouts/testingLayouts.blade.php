87ac7466d4927659d8187cd418e996864f0c3c74
Ram Emerson Goria <goria.reg@gmail.com>
2024-12-30 02:11:55 +0800

    testing: 1st section

--- resources/views/UATWEB/layouts/testingLayouts.blade.php
+++ resources/views/UATWEB/layouts/testingLayouts.blade.php
@@ -22,7 +22,7 @@
     <link rel="preload" href="{{ asset('images/homepage/homepage1-w.webp') }}" as="image" type="image/webp">
 
     <!-- Preload the critical styles CSS -->
-    <link rel="preload" href="{{ asset('css/style.css') }}" as="style">
+    <link rel="preload" href="{{ asset('css/style.css') }}" as="style" onload="this.rel='stylesheet'">
 
     <!-- Minified Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
@@ -99,7 +99,8 @@
     </style>
 
     <!-- Main CSS file with "onload" for efficient loading -->
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}" media="print" onload="this.media='all'; this.onload=null;">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}" media="print" onload="this.media='all'">
+    <noscript><link rel="stylesheet" href="{{ asset('css/style.css') }}"></noscript>
     @yield('css')
 </head>
 
