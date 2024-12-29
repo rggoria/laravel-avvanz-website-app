824869c1c6288dc21c246526e3ec22cb60776d68
Ram Emerson Goria <goria.reg@gmail.com>
2024-12-29 16:31:54 +0800

    testing: navbar js

--- resources/views/UATWEB/layouts/testingLayouts.blade.php
+++ resources/views/UATWEB/layouts/testingLayouts.blade.php
@@ -38,6 +38,11 @@
 
     @yield('content')
 
+    <!-- jQuery CDN -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+    <!-- Custom JS -->
+    <script src="{{ asset('js/index.js') }}" defer></script>
 </body>
 
 </html>
