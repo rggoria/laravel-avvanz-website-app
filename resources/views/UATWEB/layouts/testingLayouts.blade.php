61de3bcfdcb2e77f09ea12182641429dcc35229a
Ram Emerson Goria <goria.reg@gmail.com>
2024-12-31 15:32:12 +0800

    testing: navbar

--- resources/views/UATWEB/layouts/testingLayouts.blade.php
+++ resources/views/UATWEB/layouts/testingLayouts.blade.php
@@ -5,22 +5,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
-    <meta name="description"
-        content="Our Multi-award winning background checks platform ScreenGlobal will help you mitigate risk due to wrongful hires.">
+    <meta name="description" content="Our Multi-award winning background checks platform ScreenGlobal will help you mitigate risk due to wrongful hires.">
     
     <!-- Website Icon -->
     <link rel="icon" type="image/x-icon" href="{{ asset('images/homepage/avvanz.ico') }}"/>
 
-    <!-- Bootstrap CDN -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-    <!-- Respond.io CDN -->
-    <script id="respondio__widget" src="https://cdn.respond.io/webchat/widget/widget.js?cId=6850f5d54cbfd25be536b0b59847be5"></script>
-
+    <!-- Preload CSS to speed up load -->
     <link rel="preload" href="{{ asset('css/style.css') }}" as="style">
 
-    <!-- Normal link to apply the CSS (after preloading) -->
+    <!-- Inline critical CSS (this will only contain the styles needed to render the initial view) -->
+    <style>
+        body { font-family: Arial, sans-serif; margin: 0; padding: 0; }
+        h1 { color: #333; }
+        /* Add any other critical CSS you need for above-the-fold content here */
+    </style>
+
+    <!-- Bootstrap CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <!-- Normal link to apply the full CSS (after preloading) -->
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 
     <style>
@@ -34,7 +38,6 @@
             --seaGreen: #3c8d5c;
             --antiFlashWhite: #f2f2f2;
             --brightGray: #eaf1ee;
-
             --blackSqueeze: #e9f4f6;
             --linkWater: #d8e8f3;
             --givry: #f7e6c5;
@@ -42,6 +45,7 @@
             --dustStorm: #e9d6d5;
             --mojo: #bf3d36;
         }
+
         /* Floating Section */
         .floating-btn {
             position: fixed;
@@ -134,11 +138,14 @@
     <!-- Floating Button -->
     @include('UATWEB.partials.floating')
 
-    <!-- jQuery CDN -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <!-- jQuery CDN (Defer loading) -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" defer></script>
 
-    <!-- Bootstrap JavaScript -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Bootstrap JavaScript (Defer loading) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" defer></script>
+
+    <!-- Respond.io script (Defer loading) -->
+    <script id="respondio__widget" src="https://cdn.respond.io/webchat/widget/widget.js?cId=6850f5d54cbfd25be536b0b59847be5" defer></script>
 
     <script>
         document.addEventListener('DOMContentLoaded', function() {
@@ -180,6 +187,6 @@
     </script>
 
     <!-- Custom JS -->
-    <script src="{{ asset('js/index.js') }}"></script>
+    <script src="{{ asset('js/index.js') }}" defer></script>
 </body>
 </html>
